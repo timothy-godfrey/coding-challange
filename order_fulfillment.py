7e87655be442784e54ac5c6049973e68e283c9d7
tim <timothy.godfrey@gmail.com>
2022-04-06 12:05:35 +1000

    Implement 'quantity on hand' logic
    If orders can be fulfilled go through each of the items and substract 
    them from the quantity on hand.

--- order_fulfillment.py
+++ order_fulfillment.py
@@ -1,10 +1,10 @@
 import json
 import pdb
 
-def list_to_dict(product_list):
+
+def products_to_dict(product_list):
     inventory = {}
     for product in product_list:
-        # pdb.set_trace()
         inventory[product['productId']] = product
 
     return inventory
@@ -13,12 +13,44 @@ def list_to_dict(product_list):
 def process_orders():
     pass
 
+
 #  Main process
 data_file = open('data.json')
 data = json.load(data_file)
 
-inventory = list_to_dict(data['products'])
-print(inventory.keys())
-print(inventory.values())
-
+inventory = products_to_dict(data['products'])
 orders = data['orders']
+unfulfillable = []
+
+# Check that an order can be fulfilled completetly before fulfilling any
+# individual items and adjust stock levels
+for order in orders:
+    print(order['orderId'])
+    can_fulfill = True
+    id = None
+    quantity = None
+    for item in order['items']:
+        on_hand  = inventory[item['productId']]['quantityOnHand']
+        id = item['productId']
+        quantity = item['quantity']
+        print(f'\t{id} x {quantity} - on hand: {on_hand}')
+
+        if item['quantity'] > on_hand:
+            can_fulfill = False
+
+    print(f'can fulfull: {can_fulfill}')
+
+    # adjust the stock levels, create a list of unfulfillable IDs
+    if can_fulfill:
+        for item in order['items']:
+            id = item['productId']
+            quantity = item['quantity']
+            on_hand  = inventory[id]['quantityOnHand']
+            inventory[id]['quantityOnHand'] = on_hand - quantity
+    else:
+        order['status'] = 'Unfulfillable'
+        unfulfillable.append(order['orderId'])
+
+print('unfillable')
+for id in unfulfillable:
+    print(f'{id}')
